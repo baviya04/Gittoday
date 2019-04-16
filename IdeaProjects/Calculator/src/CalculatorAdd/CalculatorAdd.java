8fe0daa330749210297e5124d7cf602d25031787
baviya04 <baviya.sivabalan@gmail.com>
2019-04-16 12:36:07 +0100

    my msg

---
+++ IdeaProjects/Calculator/src/CalculatorAdd/CalculatorAdd.java
@@ -0,0 +1,22 @@
+package CalculatorAdd;
+
+public class CalculatorAdd {
+
+    //int a=6;
+    //int b=8;
+    int c;
+    public static void main(String args[])
+    {
+
+        CalculatorAdd CalObj=new CalculatorAdd();
+        int cV = CalObj.add();
+        System.out.print(" The addition is:" + cV);
+         }
+
+    public int add()
+    {
+        //Object a;
+        int c = a + b;
+        return c;
+    }
+}
